5c31c8082c0162d1b89b6330466bee8ab9a030ad
Souhaib LAANANI <sohaib.laanani@gmail.com>
2022-08-24 02:42:19 +0100

    Update the lecture page so the video and tabs change on lecture change

--- src/components/LecturePage/Lectures.tsx
+++ src/components/LecturePage/Lectures.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FiCheck, FiClock } from "react-icons/fi";
 import styles from "../../styles/LecturePage/Lectures.module.css";
 import classNames from "classnames";
+import durationToString from "helpers/durationToString";
 
 type Props = {
   data: Array<{
@@ -9,6 +10,8 @@ type Props = {
     duration: number;
     progress: number;
   }>;
+  setOpenLecture: (index: number) => void;
+  openLecture: number;
 };
 
 const ProgressIcon = (props: { progress: number; size: number }) => {
@@ -57,16 +60,8 @@ ProgressIcon.defaultProps = {
 };
 
 const Lectures = (props: Props) => {
-  const { data } = props;
+  const { data, setOpenLecture, openLecture } = props;
   const totalDuration = data.reduce((acc, cur) => acc + cur.duration, 0);
-
-  const durationToString = (duration: number) => {
-    const minutes = Math.floor(duration / 60);
-    const seconds = Math.floor(duration % 60);
-    const minutesString = minutes > 0 ? `${minutes} min, ` : "";
-    const secondsString = seconds > 0 ? `${seconds} sec` : "";
-    return `${minutesString}${secondsString}`;
-  };
 
   return (
     <div className={styles.Lectures}>
@@ -86,11 +81,13 @@ const Lectures = (props: Props) => {
       <div className={styles.lecturesContainer}>
         {data.map((lecture, index) => (
           <div
+            onClick={() => setOpenLecture(index)}
             className={classNames(styles.lecture, {
               [styles.lectureActive]:
                 lecture.progress < 1 && lecture.progress > 0,
               [styles.lectureCompleted]: lecture.progress === 1,
               [styles.lectureNotStarted]: lecture.progress === 0,
+              [styles.openLecture]: index === openLecture,
             })}
             key={index}
           >
