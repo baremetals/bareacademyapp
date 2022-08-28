e75176c74e100e3797983e40cd50a774757ca88e
Souhaib LAANANI <sohaib.laanani@gmail.com>
2022-08-28 21:03:24 +0100

    Take quiz dialog

--- src/components/Home/TakeQuizDialog.tsx
+++ src/components/Home/TakeQuizDialog.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
 import React from "react";
+import styles from "../../styles/Home/TakeQuizDialog.module.css";
 
 interface Props {}
 
 const TakeQuizDialog = (props: Props) => {
-  return <div>test</div>;
+  return (
+    <div className={styles.TakeQuizDialog}>
+      <div className={styles.TakeQuizImage}>
+        <Image src={require("./assets/takequiz.png")} alt="takequiz" />
+      </div>
+      <div className={styles.TakeQuizText}>Haven't decided what to study?</div>
+      <div className={styles.button}>Start quiz</div>
+    </div>
+  );
 };
 
 export default TakeQuizDialog;
